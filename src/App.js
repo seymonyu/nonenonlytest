7083f4ff2cf34e9146784cfe37fea62ccac6d268
seymonyu <seyma.firat@skinvision.com>
2021-02-07 20:52:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <div class="main-placeholder">
+        <img class = "slide-logo" src="./media/LOGO-NN-CINZEL.jpg"></img>
+        <h1 class="main-header">
+          
+          COMING SOON
+        </h1>
+        </div>
+        <img class = "slide-logo" src="./media/LOGO-NN-CINZEL.jpg"></img>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
